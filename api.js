9fe9001a70abd450ec6f5da7361c8bf8e0e13fbd
trichards140 <44330970+trichards140@users.noreply.github.com>
2020-02-16 13:24:14 -0500

    tiny fix

--- api.js
+++ api.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
 app.use(bodyParser.json({limit: '5mb'}));
 
 // Express Middleware Setup
-router.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://crabrr.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
