de1b446027c3080e823626c827d42c04e7f9d56a
trichards140 <44330970+trichards140@users.noreply.github.com>
2020-02-16 13:23:35 -0500

    add cors all

--- api.js
+++ api.js
@@ -43,7 +43,16 @@ app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
 app.use(bodyParser.json({limit: '5mb'}));
 
 // Express Middleware Setup
-app.use(cors());
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://crabrr.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
 
 // Some Basic Routes To Offer Basic Functionality
 
--- routes/primary.js
+++ routes/primary.js
@@ -28,7 +28,16 @@ const authVerification = require('../middleware/checkauth.js');
 
 // Route Setup
 // Express Middleware Setup
-router.use(cors());
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://crabrr.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
 router.use(cookieParser());
 
 // Check For User Auth - If The request makes it past this point, it contains a valid authorization
